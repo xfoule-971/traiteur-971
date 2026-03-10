4ec71fe6f2ff733fca493cef8b7d5494984e9a5e
xfoule <x.foule@gmail.com>
2026-03-10 17:32:16 -0400

    add server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-
 require("dotenv").config();
-const app = require("./app");
 
-const PORT = 4000;
+const app=require("./app");
+
+const PORT=process.env.PORT || 4000;
 
 app.listen(PORT,()=>{
-console.log("API traiteur 971 lancée sur http://localhost:"+PORT);
+
+    console.log(`API traiteur lancé sur ${PORT}`);
+
 });
