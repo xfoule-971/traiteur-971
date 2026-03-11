ba9ccb5867bd9aa131ea7d9cf1476497ae97fb9a
xfoule <x.foule@gmail.com>
2026-03-11 15:24:59 -0400

    add meta page évènement

--- frontend/src/pages/Evenement.jsx
+++ frontend/src/pages/Evenement.jsx
@@ -1,8 +1,17 @@
+import { Helmet } from "react-helmet";
+
 const Evenement = () => {
     return (
-        <main>
-
-        </main>
+        <>
+            <Helmet>
+                <title>traiteur-karukéra</title>
+                {/*La description aux moteurs de recherche*/}
+                <meta name="description" 
+                content="Réussissez tous vos évènements en Guadeloupe avec notre service traiteur.
+                Du cocktail au dîner assis, nous gérons tout pour vous. Réservez vite!" 
+                />
+            </Helmet>
+        </>
     );
 };
 export default Evenement;
